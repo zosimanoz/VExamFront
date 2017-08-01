6ae72079f48e863cd62f63fb2f615a58fcee0ffa
Manoj Joshi <contact.me.manoz@gmail.com>
2017-08-01 18:47:18 +0545

    department reducers set up for edit and fetch action

--- vexam/src/reducers/departments.reducer.js
+++ vexam/src/reducers/departments.reducer.js
@@ -1,4 +1,6 @@
 import { SET_DEPARTMENTS } from '../actions/departments.action';
+import { ADD_DEPARTMENT } from '../actions/departments.action';
+import { GET_DEPARTMENTBYID } from '../actions/departments.action';
 
 
 export default function games(state = [], action = {}) {
@@ -6,6 +8,37 @@ export default function games(state = [], action = {}) {
 
         case SET_DEPARTMENTS: {
             return action.departments;
+            break;
+        }
+
+        case ADD_DEPARTMENT: {
+            return [
+                ...state,
+                action.department
+            ]
+            break;
+        }
+
+        case GET_DEPARTMENTBYID: {
+            const index = state.findIndex(item => item.DepartmentId === action.department.DepartmentId);
+            console.log("idnx",index);
+            if (index > -1) {
+                return state.map(item => {
+                    if (item.DepartmentId === action.department.DepartmentId) {
+                        console.log('here found');
+                        return action.department;
+                    } else {
+                        console.log('not found');
+                        return item;
+                    }
+                })
+            } else {
+                console.log('new dat found');
+                return [
+                    ...state,
+                    action.department
+                ]
+            }
             break;
         }
 
