098282e14367aa908b0cf149ef4cd4cfc401fc8b
Manoj Joshi <contact.me.manoz@gmail.com>
2017-08-28 17:17:56 +0545

    flashmessage doen

---
+++ vexam/src/actions/flashMessage.action.js
@@ -0,0 +1,20 @@
+export const ADD_FLASH_MESSAGE = 'ADD_FLASH_MESSAGE'
+export const DELETE_FLASH_MESSAGE = 'DELETE_FLASH_MESSAGE'
+
+export function addFlashMessage(message) {
+  return {
+    type: ADD_FLASH_MESSAGE,
+    payload: {
+        message : message
+    }
+  }
+}
+
+export function deleteFlashMessage(id) {
+  return {
+    type: DELETE_FLASH_MESSAGE,
+    payload: {
+        id : id
+    }
+  }
+}
--- vexam/src/reducers/auth.reducer.js
+++ vexam/src/reducers/auth.reducer.js
@@ -1,9 +1,13 @@
-import { SET_CURRENT_USER } from '../actions/auth.action';
+import { SET_CURRENT_USER, SIGNIN_USER, SIGNIN_USER_SUCCESS,
+SIGNIN_USER_FAILURE,LOGOUT_USER,ME_FROM_TOKEN,ME_FROM_TOKEN_SUCCESS,ME_FROM_TOKEN_FAILURE,
+ RESET_TOKEN} from '../actions/auth.action';
 
 
 const initialState = {
-    isAuthenticated: false,
-    user: {}
+    authenticated: false,
+    status: null,
+    user: {},
+    errors: null
 };
 
 
@@ -11,13 +15,13 @@ const initialState = {
 export default function authReducer(state = initialState, action = {}) {
     switch (action.type) {
 
-        case SET_CURRENT_USER: {
-            return {
-                isAuthenticated: true,
-                user: action.user
-            };
-            break;
-        }
+        case SIGNIN_USER:// sign in user,  set loading = true and status = signin
+            return { ...state, user: null, authenticated: false, status:'signin', errors:null }; 
+        case SIGNIN_USER_SUCCESS://return authenticated user,  make loading = false and status = authenticated
+            return { ...state, user: action.payload.user, authenticated: true, status:'authenticated', errors:null }; //<-- authenticated
+        case SIGNIN_USER_FAILURE:// return error and make loading = false
+            return { ...state, user: null, status:'signin', authenticated: false, errors:action.payload.error };
+
 
         default: return state;
     }
